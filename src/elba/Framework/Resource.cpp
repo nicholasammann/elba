c1d5c342a91d1919ebb49d4fb7bc601284014029
nicholasammann <ammannnicholas@gmail.com>
2018-03-29 16:37:31 -0700

    resource manager files

---
+++ src/elba/Framework/Resource.cpp
@@ -0,0 +1,21 @@
+#include "Framework/Resource.hpp"
+
+/**
+* \file Resource.cpp
+* \author Nicholas Ammann
+* \date 3/29/2018
+* \brief Member function definitions for Resource.
+*/
+
+namespace Elba
+{
+  Resource::Resource(std::string identifier) : mIdentifier(identifier)
+  {
+
+  }
+
+  Resource::~Resource()
+  {
+
+  }
+}
---
+++ src/elba/Framework/ResourceManager.hpp
@@ -0,0 +1,45 @@
+/**
+* \file ResourceManager.hpp
+* \author Nicholas Ammann
+* \date 3/29/2018
+* \brief Class definition for ResourceManager.
+*/
+
+#pragma once
+
+#include "Framework/Resource.hpp"
+
+namespace Elba
+{
+  /**
+  * \brief Handles loading and unloading of assets.
+  */
+  class ResourceManager
+  {
+  public:
+
+    /**
+    * \brief Constructor
+    */
+    ResourceManager();
+
+    /**
+    * \brief Destructor
+    */
+    ~ResourceManager();
+
+    /**
+    * \brief Loads the mesh if it hasn't been loaded already, otherwise
+    *        returns the already loaded mesh.
+    * \param name The name of the mesh to be loaded.
+    * \return A pointer to the mesh.
+    */
+    Resource* LoadMesh(std::string name);
+
+  private:
+
+    std::map<std::string, Resource*> mMeshes;
+
+  };
+
+}
