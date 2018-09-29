2d281f50321ab9195037f14aeb624cf1b7c68a8d
nicholasammann <ammannnicholas@gmail.com>
2018-09-28 18:24:56 -0700

    commit before taking apart OpenGLTexture class

---
+++ src/Elba/Graphics/OpenGL/CS370/CS370_Texture.hpp
@@ -0,0 +1,22 @@
+#include "Elba/Graphics/OpenGL/OpenGLTexture.hpp"
+
+/*
+namespace Elba
+{
+
+class CS370_Texture : public OpenGLTexture
+{
+public:
+  CS370_Texture();
+
+
+private:
+
+  void LoadPPM(std::string path);
+  void InsertPPMToken(std::ofstream& file, int token, int& tokenCount);
+
+};
+
+}*/
+
+
