bcbaa486f1aec3297fb17ed0c965624ecd3534dd
Gabryelle Chamberlin <gabryelle.chamberlin@gmail.com>
2018-10-06 20:02:03 -0700

    no more gl_repeat

--- src/Elba/Graphics/OpenGL/OpenGLMesh.cpp
+++ src/Elba/Graphics/OpenGL/OpenGLMesh.cpp
@@ -251,6 +251,7 @@ unsigned int OpenGLMesh::LoadBMP(const char* aFile, std::string aDir)
 
     glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_BGR, GL_UNSIGNED_BYTE, data);
 
+
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
 
