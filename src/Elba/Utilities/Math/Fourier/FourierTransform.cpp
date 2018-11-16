bb727c1757339bb7883d84fb0fffb68cafd6d823
nicholas ammann <ammannnicholas@gmail.com>
2018-11-15 23:14:42 -0800

    ugh, fourier

--- src/Elba/Utilities/Math/Fourier/FourierTransform.cpp
+++ src/Elba/Utilities/Math/Fourier/FourierTransform.cpp
@@ -16,22 +16,36 @@ ComplexNumber::ComplexNumber(int r, int i)
 void Direct(const SpatialImage& input, FrequencyImage& output)
 {
   double exp = 2.0 * pi;
-  output.resize(input.size());
+  int N = input.size();
+  output.resize(N);
 
-  for (int freqY = 0; freqY < input.size(); ++freqY)
+  // for each element in the frequency domain
+  for (int freqY = 0; freqY < N; ++freqY)
   {
-    output[freqY].resize(input[freqY].size());
+    int M = input[freqY].size();
+    output[freqY].resize(M);
 
-    for (int freqX = 0; freqX < input[freqY].size(); ++freqX)
+    for (int freqX = 0; freqX < M; ++freqX)
     {
       output[freqY][freqX] = ComplexNumber(0, 0);
 
-      for (int spatialY = 0; spatialY < input.size(); spatialY++)
+      // for each element in the spatial image
+      for (int spatialY = 0; spatialY < N; spatialY++)
       {
-        for (int spatialX = 0; spatialX < input[freqY].size(); spatialX++)
+        for (int spatialX = 0; spatialX < M; spatialX++)
         {
-          output[freqY][freqX].real += input[spatialY][spatialX] * cos(exp * (freqY * spatialY + freqX * spatialX));
-          output[freqY][freqX].imaginary -= input[spatialY][spatialX] * sin(exp * (freqY * spatialY + freqX * spatialX));
+          double rowTerm = (exp *  static_cast<double>(freqX * spatialX)) / static_cast<double>(M);
+          double cosRow = cos(rowTerm);
+          double sinRow = -sin(rowTerm);
+
+          double colTerm = (exp * static_cast<double>(freqY * spatialY)) / static_cast<double>(N);
+          double cosCol = cos(colTerm);
+          double sinCol = -sin(colTerm);
+
+          double term = input[spatialY][spatialX];
+
+          output[freqY][freqX].real += term * (cosRow * cosCol + sinRow * sinCol);
+          output[freqY][freqX].imaginary += term * (sinCol * cosRow + sinRow * cosCol);
         }
       }
     }
@@ -41,35 +55,46 @@ void Direct(const SpatialImage& input, FrequencyImage& output)
 void Separable(const SpatialImage& input, FrequencyImage& output)
 {
   FrequencyImage temp;
-  temp.resize(input.size());
+  size_t colSize = input.size();
+  temp.resize(colSize);
 
-  for (int y = 0; y < input.size(); ++y)
+  for (int y = 0; y < colSize; ++y)
   {
-    double exp = (2.0 * pi) / static_cast<double>(input[y].size());
-    temp[y].resize(input[y].size());
-    for (int x = 0; x < input[y].size(); ++x)
+    size_t rowSize = input[y].size();
+    double exp = (2.0 * pi) / static_cast<double>(rowSize);
+    temp[y].resize(rowSize);
+
+    for (int v = 0; v < rowSize; ++v)
     {
-      temp[y][x] = ComplexNumber(0, 0);
-      temp[y][x].real += input[y][x] * cos(exp * x * y);
-      temp[y][x].imaginary -= input[y][x] * sin(exp * x * y);
+      temp[y][v] = ComplexNumber(0, 0);
+
+      for (int x = 0; x < rowSize; ++x)
+      {
+        temp[y][v].real += input[y][x] * cos(exp * x * v);
+        temp[y][v].imaginary -= input[y][x] * sin(exp * x * v);
+      }
     }
   }
 
   double exp = (2.0 * pi) / static_cast<double>(input.size());
   output.resize(input.size());
-  for (int y = 0; y < input.size(); ++y)
+  for (int u = 0; u < input.size(); ++u)
   {
-    output[y].resize(input[y].size());
-    for (int x = 0; x < input[y].size(); ++x)
+    output[u].resize(input[u].size());
+    for (int v = 0; v < input[u].size(); ++v)
     {
-      output[y][x] = ComplexNumber(0, 0);
-      output[y][x].real += cos(exp * x * y) * temp[y][x].real;
-      output[y][x].imaginary -= sin(exp * x * y) * temp[y][x].imaginary;
+      output[u][v] = ComplexNumber(0, 0);
+      
+      for (int x = 0; x < input[u].size(); ++x)
+      {
+        output[u][v].real = (cos(exp * x * y) * temp[y][x].real - sin(exp * x * y) * temp[y][x].imaginary);
+        output[u][v].imaginary = (cos(exp * x * y) * temp[y][x].imaginary + temp[y][x].real * -sin(exp * x * y));
+      }
     }
   }
 }
 
-void Fast(const SpatialImage & input, FrequencyImage & output)
+void Fast(const SpatialImage& input, FrequencyImage& output)
 {
 
 }
